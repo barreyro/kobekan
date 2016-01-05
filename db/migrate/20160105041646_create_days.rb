b381da164548e777d7138b4c160ff4fb47550eb3
Stephanie Barreyro <stephanie@recruitifi.com>
2016-01-04 23:40:49 -0500

    Create Days migration

--- db/migrate/20160105041646_create_days.rb
+++ db/migrate/20160105041646_create_days.rb
@@ -1,6 +1,11 @@
 class CreateDays < ActiveRecord::Migration
   def change
     create_table :days do |t|
+      t.date    :date
+      t.string  :game_id
+      t.integer :losses
+      t.string  :team_id
+      t.integer :wins
 
       t.timestamps null: false
     end
