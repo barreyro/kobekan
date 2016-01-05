86103880202bb495d91f2cac43ae293272d7c6f6
Stephanie Barreyro <stephanie@recruitifi.com>
2016-01-04 23:34:15 -0500

    Create Games migration

--- db/migrate/20160105041519_create_games.rb
+++ db/migrate/20160105041519_create_games.rb
@@ -1,6 +1,15 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
+      t.date    :date
+      t.string  :game_id
+      t.string  :location
+      t.string  :team_home_id
+      t.string  :team_visitor_id
+      t.integer :score_home
+      t.integer :score_visitor
+      t.string  :season
+      t.boolean :victory
 
       t.timestamps null: false
     end
