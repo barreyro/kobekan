bc60f4fa1e176efbbcbc8bd165e6bc5cd87c9c9f
Stephanie Barreyro <stephanie@recruitifi.com>
2016-01-04 23:30:06 -0500

    Create Teams migration

--- db/migrate/20160105041635_create_teams.rb
+++ db/migrate/20160105041635_create_teams.rb
@@ -1,6 +1,10 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
+      t.text :city
+      t.text :name
+      t.text :state
+      t.text :team_id
 
       t.timestamps null: false
     end
